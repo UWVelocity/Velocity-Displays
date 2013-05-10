884dc66366030a66e203f98b769d257109746931
Christophe Biocca <christophe.biocca@gmail.com>
2013-05-10 12:03:58 -0400

    Removes unused Page class.

--- server.rb
+++ server.rb
@@ -5,36 +5,6 @@ require 'sinatra/json'
 require 'multi_json'
 require 'yaml'
 
-class Page
-  #For when I actually get the thing to automatically schedule things, 
-  #I don't have time yet, so we're just going to schedule manually for now.
-
-  DICTIONARY_KEYS = [:url, :duration, :refresh]
-
-  attr_reader *DICTIONARY_KEYS
-
-  def initialize(dict)
-    DICTIONARY_KEYS.each do |key|
-      instance_variable_set(key, dict[key.to_s])
-    end
-  end
-
-  def density 
-    # the percentage of an infinite sequence that this page
-    # would take up if it had enough space to do so.
-    duration.to_f/refresh
-  end
-
-  def adjust(factor)
-    # scales density by inversely scaling refresh period
-    refresh /= factor
-  end
-
-  def to_json
-    super(Hash[DICTIONNARY_KEYS.collect{|k| self.send(k)}])
-  end
-end
-
 schedule = File.open(File.join(File.dirname(__FILE__), 'sites.yml'), 'r'){|f|YAML.load(f)}["schedule"]
 
 get '/' do
